4123d8d48667f6d1bab7e906800b661768f16744
KarenNR <74948680+KarenNR@users.noreply.github.com>
2025-01-23 22:10:07 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+  let name = "Karen";
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  Contact Us
+  First Name
+  This field is required
+
+  Last Name
+  This field is required
+
+  Email Address
+  Please enter a valid email address
+  This field is required
+
+  Query Type
+  General Enquiry
+  Support Request
+  Please select a query type
+
+  Message
+  This field is required
+
+  I consent to being contacted by the team
+  To submit this form, please consent to being contacted
+
+  Submit
+
+  Message Sent!
+  Thanks for completing the form. We'll be in touch soon!
+
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+
+  <v-btn>
+    Button
+  </v-btn>
+</template>
+
+<style>
+  .attribution { font-size: 11px; text-align: center; }
+  .attribution a { color: hsl(228, 45%, 44%); }
+</style>
