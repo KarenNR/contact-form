181955b9ac008a9cd704a23c1a6741864662f2d2
KarenNR <74948680+KarenNR@users.noreply.github.com>
2025-01-24 16:14:52 -0600

    Changed attribution to component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
   import ContactForm from './components/ContactForm.vue';
+  import AttributionBlock from './components/AttributionBlock.vue';
 </script>
 
 <template>
@@ -8,10 +9,7 @@
       <v-row class="h-100" align="center">
         <v-col cols="12">
           <ContactForm />
-          <div class="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-            Coded by <a href="#">Your Name Here</a>.
-          </div>
+          <AttributionBlock />
         </v-col>
       </v-row>
     </v-container>
@@ -20,6 +18,5 @@
 </template>
 
 <style scoped>
-  .attribution { font-size: 11px; text-align: center; }
-  .attribution a { color: hsl(228, 45%, 44%); }
+  
 </style>
---
+++ src/components/AttributionBlock.vue
@@ -0,0 +1,15 @@
+<script setup>
+  
+</script>
+
+<template>
+    <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/KarenNR" target="_blank">Karen Garza</a>.
+    </div>
+</template>
+
+<style scoped>
+  .attribution { font-size: 11px; text-align: center; }
+  .attribution a { color: hsl(228, 45%, 44%); }
+</style>
